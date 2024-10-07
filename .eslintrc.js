85c00cf23b671dff99141aff4dde83c48358290f
Tushar Jaiswal <tusharjaiswal@Tushars-MacBook-Pro.local>
2024-10-07 18:24:25 +0530

    Update changes

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // ... other configurations
-  rules: {
-    // ... other rules
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-  },
-};
